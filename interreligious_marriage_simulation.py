f8dc6d65a5b43cc56478fadd69d96d7a092beb27
Muh Alif Ahsanul Islam <alifahsanul@gmail.com>
2019-03-17 18:01:10 +0900

    add code

--- interreligious_marriage_simulation.py
+++ interreligious_marriage_simulation.py
@@ -10,6 +10,9 @@ Created on Sun Mar 17 17:28:05 2019
 Answering the question: for given probability, how will the population become when interreligion marriage happen. Muslim men can marry non muslim, but muslim women can't marry non muslim men
 """
 
+import numpy as np
+np.random.seed(42)
+
 def marry(person1, person2):
     person1.marital_status = True
     person2.marita_status = True
@@ -27,11 +30,24 @@ p1 = Person(religion='islam', gender= 'male')
 p2 = Person(religion='islam', gender = 'female')
 marry(p1, p2)
 
+def generate_random_religion(religions=['islam', 'non-islam'], p = [0.5, 0.5], n=100):
+    assert sum(p) == 1
+    assert len(religions) == len(p)
+    assert len(religions) > 1
+    religions_of_populations = []
+    #how to make a list of randomly generated religion
+    return religions_of_populations
+
 population1 = []
 n_of_person = 100
 for i in range(n_of_person):
     person = Person(religion='islam', gender= 'male')
     population1.append(person)
+
+
+
+
+
 
 
 
